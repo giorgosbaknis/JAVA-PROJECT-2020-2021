d3517fe55d6cf0c9e42fe8d047bcd3f7d008b51b
Xtheta <80396487+Xtheta@users.noreply.github.com>
2021-05-03 18:19:31 +0300

    Add files via upload
    Deleted both objects.

--- ENTITY/Main.java
+++ ENTITY/Main.java
@@ -3,9 +3,7 @@ package ENTITY;
 public class Main {
 
     public static void main(String[] args){
-        //Test Service
-        Service se = new Service("NurserySupport", "nosokoma", 5);
-        System.out.println(se.toString());
+
 
     }
 }
