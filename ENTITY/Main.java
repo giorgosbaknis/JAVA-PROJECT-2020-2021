2b1bc08b2f2c002722227a7c6e9a0aced0e7bfb4
Xtheta <80396487+Xtheta@users.noreply.github.com>
2021-05-18 18:33:08 +0300

    Add files via upload

--- ENTITY/Main.java
+++ ENTITY/Main.java
@@ -1,15 +1,41 @@
 package ENTITY;
 
+import REQUESTS.Organization;
+import USER.*;
+import REQUESTS.*;
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        /*
         //Test Service
+        Service se = new Service("NurserySupport", "nosokoma", 5);
+        Service se = new Service("NurserySupport", "nosokoma", 5);
         Service se = new Service("NurserySupport", "nosokoma", 5);
         System.out.println(se.toString());
 
         //Test Material
         Material mat = new Material("milk", "gala",2,1,2,3);
         System.out.println(mat.toString());
+         */
+        Organization org = new Organization();
+        Material milk = new Material("milk", "Milk", 1, 2, 3, 4);
+        Material sugar = new Material("sugar", "Sugar", 2, 2, 3, 4);
+        Material rice = new Material("rice", "Rice", 3, 2, 3, 4);
 
+        Service NurserySupport = new Service("NurserySupport", "nosokoma", 4);
+        Service BabySitting = new Service("BabySitting", "BabySitting", 5);
+        Service MedicalSupport = new Service("MedicalSupport", "MedicalSupport", 6);
+
+        Admin admin = new Admin("Baknis","555435123");
+        Beneficiary beneficiary1 =new Beneficiary("Kostas","6975554123",2);
+        Beneficiary beneficiary2 =new Beneficiary("Maria","6985554169",3);
+
+        Donator donator = new Donator("Babis","697666666");
+
+        beneficiary1.addRequest(new RequestDonation(milk,1));
+        beneficiary1.addRequest(new RequestDonation(BabySitting,1));
+
+        donator.add();
     }
 }
