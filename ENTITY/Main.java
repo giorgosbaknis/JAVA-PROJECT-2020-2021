2770b92c1b87565fec134d4f0448b2ccafe22b6b
Xtheta <80396487+Xtheta@users.noreply.github.com>
2021-05-26 20:02:48 +0300

    Add files via upload

--- ENTITY/Main.java
+++ ENTITY/Main.java
@@ -5,6 +5,11 @@ import REQUESTS.Organization;
 import USER.*;
 import REQUESTS.*;
 public class Main {
+    private static String currStringPhone;
+
+    public static String getCurrUserPhone() {
+        return currStringPhone;
+    }
 
     public static void main(String[] args) {
 
@@ -30,34 +35,31 @@ public class Main {
 
         Admin admin = new Admin("Baknis","555435123");
         org.setAdmin(admin);
-        //Beneficiary beneficiary1 =new Beneficiary("Kostas","6975554123",2);
+        Beneficiary beneficiary1 =new Beneficiary("Kostas","6975554123",2);
         Beneficiary beneficiary2 =new Beneficiary("Maria","6985554169",3);
         Donator donator = new Donator("Babis", "697666666");
-        Donator donator3 = new Donator("Bs", "6976632");
-        Donator donator1 = new Donator("Bas", "69766663266"); Donator donator4 = new Donator("Babi", "69766632131666");
-        Donator donator2 = new Donator("Bab", "6976666612316");
-        Beneficiary beneficiary5 =new Beneficiary("DOT","6985534534",2);
+        org.addEntity(milk);
+        org.addEntity(BabySitting);
+        org.addEntity(rice);
+
+        Organization.insertBeneficiary(beneficiary1);
+        Organization.insertBeneficiary(beneficiary2);
+        Organization.insertDonator(donator);
+
+        donator.add(new RequestDonation(milk,1000));
+        donator.add(new RequestDonation(rice,40));
+        donator.commit();
+        System.out.println(Organization.getCurrentDonations().getRdEntities());
+
+        currStringPhone = beneficiary1.getPhone();
+        beneficiary1.addRequest(new RequestDonation(milk,1));
+        beneficiary1.addRequest(new RequestDonation(rice,1));
 
 
 
-Organization.getCurrentDonations().add(new RequestDonation(milk,100));
 
-
-    //org.insertBeneficiary(beneficiary1);
-    org.insertBeneficiary(beneficiary5);
-    org.insertDonator(donator);
-
-        org.insertDonator(donator1);org.insertDonator(donator2);org.insertDonator(donator3);org.insertDonator(donator4);
-    org.addEntity(milk);
-    org.addEntity(BabySitting);
-    org.addEntity(rice);
     Menu menu = new Menu();
-    menu.check();
-
-
-
-
-
+   menu.check();
 
 
     }
